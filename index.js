8f17f054ad3dcd763964cabcf22f6dcef0b3b302
liuxin-2016 <13296712212@163.com>
2021-11-09 15:19:36 +0800

    update

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 import { intercept } from './utils/intercept';
 
 export default class Http {
-    static async get(url, params) {
-        return await intercept('GET', url, params);
+    static get(url, params) {
+        return intercept('GET', url, params);
     }
-    static async post(url, params) {
-        return await intercept('POST', url, params)
+    static post(url, params) {
+        return intercept('POST', url, params)
     }
-    static async put(url, params) {
-        return await intercept('PUT', url, params)
+    static put(url, params) {
+        return intercept('PUT', url, params)
     }
-    static async delete(url, params) {
-        return await intercept('DELETE', url, params)
+    static delete(url, params) {
+        return intercept('DELETE', url, params)
     }
-    static async patch(url, params) {
-        return await intercept('PATCH', url, params)
+    static patch(url, params) {
+        return intercept('PATCH', url, params)
     }
-    static async head(url, params) {
-        return await intercept('HEAD', url, params)
+    static head(url, params) {
+        return intercept('HEAD', url, params)
     }
 }
 
--- utils/intercept.js
+++ utils/intercept.js
@@ -1,18 +1,18 @@
 
 import { ajaxFunc } from './ajax';
-import {toast} from './toast'
+import { toast } from './toast'
 
-export async function intercept(method, url, params) {
-    return await new Promise((reslove) => {
+export function intercept(method, url, params) {
+    return new Promise((reslove) => {
         ajaxFunc(method, url, params, (data) => reslove(data))
     }).then((value) => {
         if (value.code === '200') {
             return value.data
-        }else if (value.code === '401'){
+        } else if (value.code === '401') {
             toast('登录已过期')
             window.localStorage.clear();
             window.sessionStorage.clear();
-            setTimeout(()=>window.location.reload(),1000)
+            setTimeout(function () { window.location.reload() }, 1000)
         } else {
             toast(JSON.parse(value).message)
         }
